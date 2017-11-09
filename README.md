84672421dd119112a697f9397e9492d4dd32d89f
Victor Ferreira <victor.santos@oi.net.br>
2017-11-09 01:15:58 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+#DataStorage
+
+`react-native-data-storage` is a data storage tool for React Native written on top of AsyncStorage.
+
+With `react-native-data-storage` you can set, get, remove and check for data.
+You can save integers, booleans, strings, primitive objects and arrays (anything that can be represented as in the JSON format).
+The methods always return a promise.
+
+##Installation
+
+npm install react-native-data-storage --save
+
+##Reference
+
+`set(key, value)`: sets a new value to the storage.
+`get(key)`: gets a value in the storage. Promise is rejected if key is not found. An array of keys can be passed and an object with the values will be returned.
+`has(key)`: checks whether a value has been set to the storage. Promise resolves with `true` of `false`.
+`remove(key)`: removes a value from the storage.
+`getAll()`: gets all the values in the storage as an object.
+
+##Usage
+
+```
+import DataStorage from 'react-native-data-storage';
+
+Storage.set('name','John Doe')
+.then((data)=>{
+    return Storage.has('name');
+})
+.then((data)=>{
+    if(data) return Storage.get('name');
+    else return '';
+})
+.then((name)=>{
+    console.log(name); //outputs 'John Doe';
+    return Storage.remove('name');
+})
+.then((name)=>{
+    console.log('Completed!');
+});
+```
---
+++ index.js
@@ -0,0 +1,89 @@
+import { AsyncStorage } from 'react-native';
+
+class _DataStorage{
+    constructor(){
+        this.secret = 'react-native-data-storage';
+        this.init();
+    }
+
+    init(){
+        try {
+            AsyncStorage.getItem(this.secret)
+            .then((data)=>{
+                if (data === null) this.setAll({});
+            });
+        } catch (err) {
+            Promise.reject(err);
+        }
+    }
+
+    getAll(){
+        try {
+            return AsyncStorage.getItem(this.secret)
+            .then((data)=>{
+                console.log('getAll', data)
+                if (data !== null){
+                    return JSON.parse(data);
+                }else return {};
+            });
+        } catch (err) {
+            console.log('getAll err')
+            Promise.reject(err);
+        }
+    }
+
+    setAll(data){
+        try {
+            if(typeof data !== 'string') data = JSON.stringify(data);
+            console.log('storage set data', data)
+            return AsyncStorage.setItem(this.secret, data);
+        } catch (err) {
+            console.log('err setAll', err.message)
+            return Promise.reject(err);
+        }
+    }
+
+    set(key, value){
+        return this.getAll()
+        .then((data)=>{
+            if(typeof key === 'object' && !value) data = Object.assign({},data,key);
+            else data[key] = value;
+            return this.setAll(data);
+        });
+    }
+
+    get(key){
+        return this.getAll()
+        .then((data)=>{
+            if(Array.isArray(key)){
+                var output = {};
+                for(var k in key){
+                     if(k in data) output[k] = data[k];
+                     else  throw new Error(`Key ${k} not found`);
+                }
+
+                return output;
+            }else{
+                if(key in data) return data[key];
+                else throw new Error(`Key ${key} not found`);
+            }
+        });
+    }
+
+    has(key){
+        return this.getAll()
+        .then((data)=>{
+            return key in data;
+        });
+    }
+
+    remove(key){
+        return this.getAll()
+        .then((data)=>{
+            delete data[key];
+            return this.setAll(data);
+        });
+    }
+}
+
+export default DataStorage = new _DataStorage();
